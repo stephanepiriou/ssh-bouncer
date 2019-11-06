06a2918a07af130bce36de7f5a98052bc35796d5
Stephane Piriou <stephane.piriou@zaclys.net>
2019-11-06 14:32:04 +0100

    Install Script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+make 
+
+cp ssh-bouncer /usr/local/bin/ssh-bouncer
+chown root.root /usr/local/bin/ssh-bouncer
+chmod 755 /usr/local/bin/ssh-bouncer
+
+cp ssh-bouncer.sh /usr/local/bin/ssh-bouncer.sh
+chown root.root /usr/local/bin/ssh-bouncer.sh
+chmod 755 /usr/local/bin/ssh-bouncer.sh
+
+cp ssh-bouncer.service /etc/systemd/system/ssh-bouncer.service
+chown root.root /etc/systemd/system/ssh-bouncer.service
+chmod 755 /etc/systemd/system/ssh-bouncer.service
+
+systemctl enable ssh-bouncer
+systemctl start ssh-bouncer 
+
