cf07d0570eeb34fbc22a517b386ae34731e8d2cc
Stephane Piriou <stephane.piriou@zaclys.net>
2019-11-06 16:45:40 +0100

    Return 0

--- install.sh
+++ install.sh
@@ -14,11 +14,11 @@ chmod 755 /usr/local/bin/ssh-bouncer.sh
 
 echo "#!/bin/sh
 /usr/local/bin/ssh-bouncer" >> /etc/init.d/ssh-bouncer
+chmod 755 /etc/init.d/ssh-bouncer
 
 mkdir /home/ssh-bouncer
 chmod 777 /home/ssh-bouncer
 
-chmod 755 /etc/init.d/ssh-bouncer
 
 /usr/local/bin/ssh-bouncer 
 
